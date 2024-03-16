25e9d888d1e4ba04f48860977c089a1d243989f0
Uliana <Ulianazhilatova@mail.ru>
2024-03-16 13:54:19 +0300

    Добавили Task1 Lesson2

---
+++ lesson2/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Массивы 
+
+int[] arr = new int[5]; // создали массив
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+// Console.WriteLine(arr[5]); - ошибка, тк элемента №5 нет 
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 1, 2, 3, 4, 5 }; // если элементов мало и они известны
+
+
