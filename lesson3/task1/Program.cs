901e1d086ebd1dfcfc3564f9a1de84c0fe2dad58
Uliana <Ulianazhilatova@mail.ru>
2024-03-17 11:04:12 +0300

    lesson 3

---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b; // числитель
+    int denumenator = c + d; // знаменатель
+    double result = numenator / denumenator;
+    return result;
+}
+double result = CalculateFormula(1, 2, 3, 4); // если нет аргументов, то просто ()
+Console.WriteLine(result);
